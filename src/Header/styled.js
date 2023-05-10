9518c27dac8ed7783da437bd9e7f9d8591f2fd8a
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-05-10 13:49:49 +0200

    Header component added

---
+++ src/Header/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+font-style: normal;
+font-weight: 900;
+font-size: 38px;
+line-height: 46px;
+max-width: 900px;
+margin: 10px auto;
+`;
