aff73a7cbafc396bcd214e226d1d564f00eeb709
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-05-12 14:04:36 +0200

    added Container and Section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import Header from "./Header";
+import { Container } from "./Container/styled";
+import Section from "./Section";
 
 
 function App() {
   return (
-   <>Cześć</>
+   
+    <Container>
+      <Header title= "Karolina Jasiówka" />
+      <Section title= "My skillset includes" body="React" />
+
+      <Section title= "What I want to learn next"  body="React" />
+    </Container>
   );
 }
 
