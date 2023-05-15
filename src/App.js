9156d4802757ef0544c5fcb51455ec3672b4b5a0
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-05-15 11:05:26 +0200

    Container, Header,Section and  Footer moved to features

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import Header from "./Header";
-import { Container } from "./Container/styled";
-import Section from "./Section";
+import Header from "./features/Header";
+import { Container } from "./features/Container/styled";
+import Section from "./features/Section";
+import Footer from "./features/Footer";
 
 
 function App() {
   return (
-   
+   <>
     <Container>
       <Header title= "Karolina Jasiówka" />
       <Section title= "My skillset includes" body="React" />
 
-      <Section title= "What I want to learn next"  body="React" />
+      <Section title= "What I want to learn next"  body="TypeScript" />
     </Container>
+    <Footer />
+    </>
   );
 }
 
