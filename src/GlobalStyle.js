cbb49ff976196d37e14ec8aad7f597918ceec88f
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-05-19 09:01:07 +0200

    added padding and letter-spacing to root

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -6,18 +6,21 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   *, ::after, ::before {
-   box-sizing:inherit;
+   box-sizing: inherit;
   }
 
   #root {
     font-family: 'Inter', sans-serif;
+    justify-content: center;
     color: ${({ theme }) => theme.color.mineShaft}; 
     background: ${({ theme }) => theme.color.whiteLilac};
     word-break: break-word;
+    padding: 115px 40px 110px 40px;
+    letter-spacing: 0.05em;
+    
   }
-
+  
   body {
     margin:0;
   }
-  
   `;
