e47938b12bc903f936de2c12d5acbcc611fe009c
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-05-19 08:59:11 +0200

    added photo, subheader, Info and styled to component Header

--- src/features/PersonalHomepage/Header/index.js
+++ src/features/PersonalHomepage/Header/index.js
@@ -1,9 +1,20 @@
-import { StyledHeader } from "./styled";
+import { Wrapper, StyledHeader, SubHeader, Image, Content, Info } from "./styled";
+import photo from "./images/KJ.jpg";
 
-const Header = ({title}) => (
-    <StyledHeader>
-        <h1>{title}</h1>
-    </StyledHeader>
+const Header = ({ title }) => (
+    <Wrapper>
+        <Image src={photo} alt=""></Image>
+        <Content>
+            <SubHeader>This is</SubHeader>
+            <StyledHeader>{title}</StyledHeader>
+            <Info>
+                I'm a passionate Frontend Developer in love with React,
+                currently looking for new job opportunities. I am an ambitious,
+                person who wants to constantly develop and learn new
+                technologies.
+            </Info>
+        </Content>
+    </Wrapper>
 );
 
 export default Header;
--- src/features/PersonalHomepage/Header/styled.js
+++ src/features/PersonalHomepage/Header/styled.js
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
+export const Wrapper = styled.div`
+display: grid;
+grid-template-columns: 1fr 2fr;
+grid-gap: 66px;
+align-items: center;
+margin-bottom: 63px;`
+
 export const StyledHeader = styled.header`
 font-style: normal;
 font-weight: 900;
@@ -8,3 +15,33 @@ line-height: 46px;
 max-width: 900px;
 margin: 10px auto;
 `;
+
+export const SubHeader = styled.h2`
+font-weight: 700;
+font-size:12px;
+line-height: 16px;
+text-transform: uppercase;
+color: ${({ theme }) => theme.color.slateGrey};
+margin-bottom:12px;
+`;
+
+export const Image = styled.img`
+max-width: 100%;
+height: auto;
+border-radius: 50%;
+transition:0.7s;
+
+@media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        width: 133px;
+        height: 133px;
+    }
+`;
+
+export const Content = styled.div``;
+
+export const Info = styled.p`
+font-weight: 400;
+font-size: 20px;
+line-height: 140%;
+letter-spacing: 0.05em;`
+
