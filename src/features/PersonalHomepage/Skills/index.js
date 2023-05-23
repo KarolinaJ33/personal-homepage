ff5f54f70427feaf48ef7697fedf39494f5ed4b5
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-05-23 10:43:13 +0200

    added component Skill with skillsList

---
+++ src/features/PersonalHomepage/Skills/index.js
@@ -0,0 +1,10 @@
+import { Section } from "../Section";
+import { skillsList } from "./skillsList";
+
+export const Skills = () => (
+    
+        <Section 
+            title="My skillset includes 🛠️" 
+            skills={skillsList}>
+        </Section>      
+);
