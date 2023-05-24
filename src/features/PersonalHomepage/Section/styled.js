c88e8f89b7f3bd8d35420eb0b6c08fe16d85abdf
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-05-24 09:32:58 +0200

    Improved styling for Wrapper, Header, List, and Content and adjusted responsive styles - @media

--- src/features/PersonalHomepage/Section/styled.js
+++ src/features/PersonalHomepage/Section/styled.js
@@ -1,36 +1,75 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.section`
-background-color: ${({ theme }) => theme.color.white};
-max-width: 1216px;
-height: 404px;
-box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
-border-radius: 4px;
-left: calc(50% - 328px/2 - 410px);
-top: 608px;
+    background-color: ${({ theme }) => theme.color.white};
+    box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02),
+        0px 16px 58px rgba(9, 10, 51, 0.03);
+    border-radius: 4px;
+    padding: 32px;
+    margin: 0 0 72px;
+    width: 100%;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        margin: 0 0 50px;
+        padding: 16px;
+    };
+    
 `;
 
 export const Header = styled.h2`
-margin: 32px 0 10px 22px;
-padding: 5px 5px 5px 10px;
-justify-content: space-between;
-font-style: normal;
-font-weight: 900;
-font-size: 30px;
-line-height: 36px;
-text-align: left;
+    font-weight: 900;
+    font-size: 30px;
+    line-height: 36px;
+    letter-spacing: 0.05em;
+    padding-bottom: 15px;
+    border-bottom: 1px solid ${({ theme }) => theme.color.iron};
 
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        font-size: 18px;
+        padding-bottom: 12px;
+    };
 `;
 
-export const List = styled.header`
-align-content: center;
-align-items: center;
-margin-bottom: 10px;
-border-bottom: 1px solid ${({ theme }) => theme.color.whiteLilac};
-padding: 5px 5px 5px 20px;
-justify-content: space-between;
+export const List = styled.li`
+    display: flex;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 25px;
+    letter-spacing: 0.05em;
+    list-style: none;
+    align-items: center;
+    margin: 0px;
+    color: ${({ theme }) => theme.color.slateGrey};
+    &::before {
+        content: " ";
+        background-color: ${({ theme }) => theme.color.scienceBlue};
+        display: block;
+        width: 9px;
+        height: 9px;
+        border-radius: 50%;
+        margin-right: 16px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+            height: 6px;
+            width: 6px;
+        };
+        };
+        
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        font-size: 14px;
+        line-height: 120%; // sprawdź ile to px na figmie w wersji mobilnej i ewentualnie podaj w px
+    };
 `;
 
-export const Content = styled.div`
-padding: 0 20px 15px 20px;
+export const Content = styled.ul`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    padding: 0;
+    grid-gap: 8px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        grid-template-columns: 1fr;
+        padding: 12px 0 0;
+        grid-gap: 8px;
+    };
 `;
