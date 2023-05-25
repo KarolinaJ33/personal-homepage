2d12a10f8b90f7982ec1b0fd49a10afee2d65f9a
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-05-25 08:08:44 +0200

    improved Button styled

--- src/features/PersonalHomepage/Button/styled.js
+++ src/features/PersonalHomepage/Button/styled.js
@@ -2,11 +2,11 @@ import styled from "styled-components";
 
 export const StyledButton = styled.button`
     display: flex;
-    align-items: center;
-    gap: 16px;
     flex-direction: row;
     justify-content: center;
-    font-weight: 600;
+    align-items: center;
+    gap: 16px;
+    font-size: 20px;
     padding: 12px 16px;
     height: 49px;
     border: 1px solid rgba(209, 213, 218, 0.3);
@@ -14,11 +14,13 @@ export const StyledButton = styled.button`
     border-radius: 4px;
     background-color: ${({ theme }) => theme.color.scienceBlue};
     color: ${({ theme }) => theme.color.white};
-    font-size: 20.0584px;
+    font-size: 20px;
     cursor: pointer;
     line-height: 24px;
     letter-spacing: 0.05em;
+    transition: 0.3s;
     position: absolute;
+    
 
     &:hover {
         box-shadow: 2px -2px 0px #8cc2ff, -2px 2px 0px #8cc2ff,
@@ -31,6 +33,7 @@ export const StyledButton = styled.button`
 
     @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
         height: 46px;
+        font-size: 18px;
     }
 `;
 
