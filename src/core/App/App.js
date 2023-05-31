eb2004de7c5a00a8d5c6fb58e38bce8d668935d4
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-05-31 10:29:41 +0200

    refactored App - updated location, added Provider, GlobalStyle, Redux store

---
+++ src/core/App/App.js
@@ -0,0 +1,17 @@
+import { PersonalHomepage } from "../../features/PersonalHomepage";
+import { theme } from "./theme";
+import { GlobalStyle } from "./GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
+import store from "../store";
+
+export const App = () => (
+    <Provider store={store}>
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <PersonalHomepage />
+        </ThemeProvider>
+    </Provider>
+);
+
+export default App;
