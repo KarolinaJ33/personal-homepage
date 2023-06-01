f032a13d8feeef9aaa4b5f859d9da3980c7225e5
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-06-01 10:16:54 +0200

    add fetching data from API and request state support

--- src/features/PersonalHomepage/Portfolio/index.js
+++ src/features/PersonalHomepage/Portfolio/index.js
@@ -1,20 +1,31 @@
 import { Wrapper, Header, SubHeader, Icon } from "./styled";
 import { PortfolioContent } from "./PortfolioContent";
-
+import { useDispatch, useSelector } from "react-redux"; 
+import { useEffect } from "react";
+import { fetchPortfolio, selectPortfolio, selectPortfolioState } from "../../portfolioSlice";
 
 export const Portfolio = () => {
-  
-    return (
+    const dispatch = useDispatch();
+    
+    const portfolioState = useSelector(selectPortfolioState);
+    const portfolio = useSelector(selectPortfolio)
 
-    <Wrapper>
-        <Icon />
-        <Header>Portfolio</Header>
-        <SubHeader>My recent projects</SubHeader>
-        <PortfolioContent
-        />
-    </Wrapper>
-);
-    };
+    useEffect(() => {
+        dispatch(fetchPortfolio())
+    }, [])
+    
+    return (
+        <Wrapper>
+            <Icon />
+            <Header>Portfolio</Header>
+            <SubHeader>My recent projects</SubHeader>
+            <PortfolioContent
+                portfolio={portfolio}
+                portfolioState={portfolioState}
+            />
+        </Wrapper>
+    );
+};
 
 export default Portfolio;
 
