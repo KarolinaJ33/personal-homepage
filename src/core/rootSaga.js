9b52045a753dc1729e047a120ffe6dd0a89c2679
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-06-01 09:47:21 +0200

    root Saga Created

---
+++ src/core/rootSaga.js
@@ -0,0 +1,9 @@
+import { all } from "redux-saga/effects";
+import { watchFetchPortfolio } from "../features/portfolioSaga";
+
+export default function* rootSaga() {
+    yield all([
+        watchFetchPortfolio(),
+
+    ]);
+};
