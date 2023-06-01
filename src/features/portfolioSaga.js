ed243fe645b469dc74701b19454af888bce587a7
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-06-01 10:05:09 +0200

    portfolioSaga created

---
+++ src/features/portfolioSaga.js
@@ -0,0 +1,21 @@
+import { delay, put, call, takeLatest } from "redux-saga/effects";
+import { fetchPortfolio, fetchError, fetchRepositories  } from "./portfolioSlice";
+import { portfolioAPI } from "./portfolioAPI";
+
+
+const DELAY_ACTION = 2000;
+
+function* fetchPortfolioHandler() {
+    try {
+        yield delay(DELAY_ACTION);
+        const portfolio = yield call(portfolioAPI);
+        yield put(fetchRepositories(portfolio));
+    } catch (error) {
+    yield put(fetchError());
+    };
+
+};
+
+export function* watchFetchPortfolio() {
+    yield takeLatest(fetchPortfolio.type, fetchPortfolioHandler)
+};
