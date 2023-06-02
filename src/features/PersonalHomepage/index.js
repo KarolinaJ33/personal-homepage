fc21d7f4be02cf33ce86a789ebefe915bc613162
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-06-02 10:55:04 +0200

    added import for Container component in PersonalHomepage

--- src/features/PersonalHomepage/index.js
+++ src/features/PersonalHomepage/index.js
@@ -1,18 +1,14 @@
-
 import { Header } from "./Header";
 import { Skills } from "./Skills";
 import { Footer } from "./Footer";
 import { Portfolio } from "./Portfolio";
-
-
+import { Container } from "../../common/Container";
 
 export const PersonalHomepage = () => (
-    <>
+    <Container>
         <Header />
         <Skills />
         <Portfolio />
         <Footer />
-    </>
+    </Container>
 ); 
-
-export default PersonalHomepage;
