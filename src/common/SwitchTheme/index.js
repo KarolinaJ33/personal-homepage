57826db07714d5452560a7e280f877386d48f5e5
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-07-19 10:27:46 +0200

    features SwitchTheme

--- src/common/SwitchTheme/index.js
+++ src/common/SwitchTheme/index.js
@@ -1,16 +1,20 @@
-import { useDispatch } from "react-redux";
-import { Wrapper, Button, Icon } from "./styled";
-import { toggleTheme } from "./themeSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { Wrapper, Button, Icon, Text, Box, IconWrapper } from "./styled";
+import { selectDarkTheme, toggleTheme } from "./themeSlice";
 
 export const SwitchTheme = () => {
     const dispatch = useDispatch();
+    const isDarkTheme = useSelector(selectDarkTheme);
 
     return (
         <Wrapper>
-            <Button 
-                 onClick={() => dispatch(toggleTheme())}
-            >Przełącz motyw
-            <Icon />
+            <Button onClick={() => dispatch(toggleTheme())}>
+                <Text>Dark mode {isDarkTheme ? "on" : "off"}</Text>
+                <Box>
+                    <IconWrapper moveToRight={isDarkTheme}>
+                        <Icon />
+                    </IconWrapper>
+                </Box>
             </Button>
         </Wrapper>
     );
--- src/common/SwitchTheme/styled.js
+++ src/common/SwitchTheme/styled.js
@@ -1,22 +1,64 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { ReactComponent as SunIcon } from "./sun.svg";
 
 export const Wrapper = styled.div`
-    align-self: self-end;
+    align-self: flex-end;
     margin: 115px 0px 0px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        margin: 32px 0 0;
+    }
 `;
 
 export const Button = styled.button`
     display: flex;
     align-items: center;
-    color: ${({ theme }) => theme.color.slateGrey};
+    color: inherit;
     background: transparent;
     font-size: 12px;
     font-weight: 700;
     line-height: 130%;
+    border: none;
+    cursor: pointer;
+    padding: 0;
+    text-transform: uppercase;
+    outline-offset: 8px;
+`;
+
+export const Text = styled.span`
+    margin: 3px 12px 0 0;
+    font-size: 12px;
+    text-transform: uppercase;
+    font-weight: 700;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        display: none;
+    }
+`;
+
+export const Box = styled.span`
+    border-radius: 13px;
+    display: flex;
+    padding: 3px;
+    width: 48px;
+    border: 1px solid ${({ theme }) => theme.colors.switchTheme.border};
+    background: ${({ theme }) => theme.colors.switchTheme.background};
+`;
+
+export const IconWrapper = styled.span`
+    border-radius: 100%;
+    display: flex;
+    background: ${({ theme }) => theme.colors.switchTheme.backgroundIcon};
+    padding: 3px;
+    transition: transform 0.3s;
+
+    ${({ moveToRight }) =>
+        moveToRight &&
+        css`
+            transform: translateX(20px);
+        `};
 `;
 
 export const Icon = styled(SunIcon)`
-    color: ${({ theme }) => theme.color.white};
-    background-color: ${({ theme }) => theme.color.mercury};
+     fill: ${({ theme }) => theme.colors.switchTheme.icon};
 `;
