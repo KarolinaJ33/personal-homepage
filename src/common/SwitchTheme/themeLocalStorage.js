961162990b91d5b68225333ccffe97f73262a672
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-07-19 10:29:51 +0200

    created localStorage

--- src/common/SwitchTheme/themeLocalStorage.js
+++ src/common/SwitchTheme/themeLocalStorage.js
@@ -1,7 +1,7 @@
 const localStorageKey = "isDarkTheme";
 
-export const saveThemeInLocalStorage = isDarkTheme =>
+export const saveThemeInLocalStorage = (isDarkTheme) =>
     localStorage.setItem(localStorageKey, JSON.stringify(isDarkTheme));
 
     export const getThemeFromLocalStorage = () =>
-    JSON.parse(localStorage.getItem(localStorageKey)) !== false;
+    JSON.parse(localStorage.getItem(localStorageKey)) || false;
