f1311cc72c38f02bfb79f9ebde33c595d3f15d9f
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-07-19 10:37:10 +0200

    configured Redux store with Saga middleware and reducers

--- src/core/store.js
+++ src/core/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
+import themeReducers from "../common/SwitchTheme/themeSlice";
 import portfolioReducers from "../features/portfolioSlice"; 
 import rootSaga from "./rootSaga";
 
@@ -8,6 +9,7 @@ const sagaMiddleware = createSagaMiddleware();
 const store = configureStore({
     reducer: {
         portfolio: portfolioReducers,
+        theme: themeReducers,
     },
     middleware: [sagaMiddleware]
 });
