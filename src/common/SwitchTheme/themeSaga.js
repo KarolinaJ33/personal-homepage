7dc88a81f5e80bda6164629cdaf650664286fd57
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-07-26 10:26:20 +0200

    saving selected theme to localstorage

--- src/common/SwitchTheme/themeSaga.js
+++ src/common/SwitchTheme/themeSaga.js
@@ -1,12 +1,12 @@
-import { select, call, takeLatest } from "redux-saga/effects";
+import { select, call, takeEvery } from "redux-saga/effects";
 import { saveThemeInLocalStorage } from "./themeLocalStorage";
 import { selectDarkTheme } from "./themeSlice";
 
 function* saveThemeInLocalStorageHandler() {
     const isDarkTheme = yield select(selectDarkTheme);
     yield call(saveThemeInLocalStorage, isDarkTheme);
-}
+};
 
 export function* watchThemeFromLocalStorage() {
-    yield takeLatest(toggleTheme.type, saveThemeInLocalStorageHandler);
-}
+    yield takeEvery("*", saveThemeInLocalStorageHandler);
+};
--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,6 +1,10 @@
 import { all } from "redux-saga/effects";
 import { watchFetchPortfolio } from "../features/portfolioSaga";
+import { watchThemeFromLocalStorage } from "../common/SwitchTheme/themeSaga";
 
 export default function* rootSaga() {
-    yield all([watchFetchPortfolio()]);
+    yield all([
+        watchFetchPortfolio(),
+        watchThemeFromLocalStorage(),
+    ]);
 }
